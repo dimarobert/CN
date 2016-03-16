a3d7054065d0346f5a85d344e52b604e86a9aec5
Dima Robert <robert.dima@dnnsharp.com>
2016-03-16 19:43:37 +0200

    Update LU. Not finished.

--- Tema2/Tema2/Program.cs
+++ Tema2/Tema2/Program.cs
@@ -13,20 +13,56 @@ namespace Tema2 {
             while (true) {
                 Console.WriteLine("1. Factorizarea LU");
                 //Console.WriteLine("2. Metoda Falsei Pozitii");
-                //Console.WriteLine("3. Metoda Coardei");
+                Console.WriteLine("3. QR");
 
                 var sel = int.Parse(Console.ReadLine());
 
                 switch (sel) {
                     case 1: ResolveLU(); break;
                     //case 2: falseiPozitii(); break;
-                    //case 3: coardei(); break;
+                    case 3: ResolveQR(); break;
                     default: continue;
                 }
                 Console.Write("Press any key to try another method...");
                 Console.ReadKey();
                 Console.WriteLine();
             }
+        }
+
+
+        static void ResolveQR() {
+            int n, p;
+
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+
+            Console.Write("p=");
+            p = int.Parse(Console.ReadLine());
+
+            Matrix<double> A = GetMatrix(n, p);
+
+            Vector<double> b = CreateVector.Dense(n, 1.0);
+
+            var At = A.Transpose();
+            var AtA = At.Multiply(A);
+
+            var Atb = At.Multiply(b);
+
+            Matrix<double> R = new DenseMatrix(n);
+
+            for (int k = 0; k < n; k++) {
+                for (int j = 0; j < k - 1; j++) {
+                    //R[j,k] = QRSum()
+                }
+            }
+
+        }
+
+        static double QRSum(Matrix<double> A, Matrix<double> Q) {
+
+
+
+            return 0.0;
         }
 
 
@@ -41,6 +77,8 @@ namespace Tema2 {
 
 
             Matrix<double> A = GetMatrix(n, p);
+
+
             //Console.WriteLine(A.ToString());
             Vector<double> b = CreateVector.Dense(n, 1.0);
 
@@ -48,6 +86,18 @@ namespace Tema2 {
             var AtA = At.Multiply(A);
 
             var Atb = At.Multiply(b);
+
+
+
+
+            var lu = AtA.LU();
+            var luSolve = lu.Solve(Atb);
+            var luRes = ComputeLUx(lu.L, lu.U, Atb, n);
+            Console.WriteLine("LU Solve: " + luSolve.ToString() + "\nLU Res:" + luRes.ToString());
+
+
+
+
 
             for (int k = 1; k <= n; k++) {
                 if (AtA.SubMatrix(0, k, 0, k).Determinant() == 0) {
@@ -63,7 +113,13 @@ namespace Tema2 {
             for (int k = 0; k < n; k++) {
 
                 for (int j = k; j < n; j++) {
-                    U[k, j] = AtA[k, j] - LUSum(L, U, 0, k - 1, k, j);
+
+                    double sum = 0.0;
+                    for (int _p = 0; _p <= k - 1; _p++) {
+                        sum += L[k, _p] * U[_p, j];
+                    }
+
+                    U[k, j] = AtA[k, j] - sum;
                 }
 
                 for (int i = k + 1; i < n; i++) {
@@ -80,16 +136,31 @@ namespace Tema2 {
             }
 
             for (int i = n - 1; i >= 0; i--) {
-                x[i] = y[i] - ySum(U, x, i + 1, n, i);
+                x[i] = y[i] - ySum(U, x, i + 1, n-1, i);
                 x[i] = x[i] / U[i, i];
             }
 
             Console.WriteLine(x.ToString());
         }
 
+        static Vector<double> ComputeLUx(Matrix<double> L, Matrix<double> U, Vector<double> b, int n) {
+            Vector<double> y = CreateVector.Dense(n, 0.0);
+            Vector<double> x = CreateVector.Dense(n, 0.0);
+
+            for (int i = 0; i < n; i++) {
+                y[i] = b[i] - ySum(L, y, 0, i - 1, i);
+            }
+
+            for (int i = n - 1; i >= 0; i--) {
+                x[i] = y[i] - ySum(U, x, i + 1, n - 1, i);
+                x[i] = x[i] / U[i, i];
+            }
+            return x;
+        }
+
         static double ySum(Matrix<double> L, Vector<double> y, int from, int to, int Li) {
             double result = 0;
-            for (int k = from; k < to; k++) {
+            for (int k = from; k <= to; k++) {
                 result += L[Li, k] * y[k];
             }
             return result;
@@ -97,7 +168,7 @@ namespace Tema2 {
 
         static double LUSum(Matrix<double> L, Matrix<double> U, int from, int to, int Li, int Uj) {
             double result = 0;
-            for (int p = from; p < to; p++) {
+            for (int p = from; p <= to; p++) {
                 result += L[Li, p] * U[p, Uj];
             }
             return result;
