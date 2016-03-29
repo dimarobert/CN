f91f6aee6124bcf4c63f86e940c5dc5d7556bcc7
Dima Robert <lord.specter@hotmail.com>
2016-03-29 22:37:18 +0300

    Added tema 3 again.

---
+++ Tema3/Tema3/DecimalMatrix.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema3
+{
+    public struct DecimalIterator
+    {
+        public int i, j;
+        public decimal value;
+    }
+
+    public delegate decimal FoldDelegate(decimal value, decimal accumulator);
+
+    public class DecimalMatrix
+    {
+
+        private decimal[,] values;
+        private int rowCount, columnCount;
+
+        public DecimalMatrix(int n) : this(n, n) { }
+        public DecimalMatrix(int n, decimal val) : this(n, n, val) { }
+
+
+        public DecimalMatrix(int n, int m) : this(n, m, 0) { }
+        public DecimalMatrix(int n, int m, decimal val)
+        {
+            rowCount = n;
+            columnCount = m;
+            values = new decimal[n, m];
+            if (val != 0)
+                Iterate(it => this[it.i, it.j] = val);
+        }
+
+        public decimal this[int i]
+        {
+            get { return values[i, 0]; }
+            set { values[i, 0] = value; }
+        }
+        public decimal this[int i, int j]
+        {
+            get { return values[i, j]; }
+            set { values[i, j] = value; }
+        }
+
+        public DecimalMatrix Transpose()
+        {
+            DecimalMatrix ret = new DecimalMatrix(columnCount, rowCount);
+
+            Iterate(it => ret[it.j, it.i] = it.value);
+
+            return ret;
+        }
+
+        public DecimalMatrix Multiply(DecimalMatrix other)
+        {
+            if (columnCount != other.rowCount)
+                throw new ArgumentException("Matrix sizes must match");
+
+            DecimalMatrix ret = new DecimalMatrix(rowCount, other.columnCount);
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < other.columnCount; j++)
+                {
+                    ret[i, j] = 0;
+                    for (int k = 0; k < columnCount; k++)
+                    {
+                        ret[i, j] += this[i, k] * other[k, j];
+                    }
+                }
+            }
+            return ret;
+        }
+
+        public DecimalMatrix SubMatrix(int startRow, int rowCount, int startColumn, int columnCount)
+        {
+            DecimalMatrix ret = new DecimalMatrix(rowCount, columnCount);
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < columnCount; j++)
+                {
+                    ret[i, j] = this[startRow + i, startColumn + j];
+                }
+            }
+
+            return ret;
+        }
+
+        public void Iterate(Action<DecimalIterator> func)
+        {
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < columnCount; j++)
+                {
+                    func(new DecimalIterator() { i = i, j = j, value = this[i, j] });
+                }
+            }
+        }
+
+        public void Iterate(Func<DecimalIterator, decimal> func)
+        {
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < columnCount; j++)
+                {
+                    this[i, j] = func(new DecimalIterator() { i = i, j = j, value = this[i, j] });
+                }
+            }
+        }
+
+        public DecimalMatrix GetColumn(int column)
+        {
+            if (column < 0 || column >= columnCount)
+                throw new ArgumentException("Column index outside bounds.");
+
+            return SubMatrix(0, rowCount, column, 1);
+        }
+
+        public DecimalMatrix GetRow(int row)
+        {
+            if (row < 0 || row >= rowCount)
+                throw new ArgumentException("Row index outside bounds.");
+
+            return SubMatrix(row, 1, 0, columnCount);
+        }
+
+        public decimal Fold(FoldDelegate func, decimal initialVal = 0)
+        {
+            decimal acc = initialVal;
+            Iterate(it =>
+            {
+                acc = func(it.value, acc);
+            });
+
+
+            return acc;
+        }
+
+        public decimal Sum()
+        {
+            decimal sum = 0;
+            Iterate(it => sum += it.value);
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            int lasti = 0;
+            Iterate(it =>
+            {
+                if (lasti != it.i)
+                {
+                    lasti = it.i;
+                    sb.Append("\r\n");
+                }
+                sb.Append($"\t{it.value}");
+            });
+            sb.Append('\n');
+            return sb.ToString();
+        }
+    }
+}
---
+++ Tema3/Tema3/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Factorizarea LU");
+                Console.WriteLine("2. Cholesky");
+                Console.WriteLine("3. QR");
+
+                var sel = int.Parse(Console.ReadLine());
+
+                switch (sel)
+                {
+                    case 1: JacobiRelaxata(); break;
+                    //case 2: ResolveCholesky(); break;
+                    //case 3: ResolveQR(); break;
+                    default: continue;
+                }
+                Console.Write("Press any key to try another method...");
+                Console.ReadKey();
+                Console.WriteLine();
+            }
+        }
+
+        static void JacobiRelaxata()
+        {
+            int n, p, err;
+
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+
+            Console.Write("p=");
+            p = int.Parse(Console.ReadLine());
+
+            Console.Write("err=");
+            err = int.Parse(Console.ReadLine());
+
+            var A = GetMatrix(n);
+            var b = new DecimalMatrix(n, 1, 1m / (n * n));
+            var x = new DecimalMatrix(n, 0m);
+
+            var norma = Enumerable.Range(0, n).ToList().Select((i) => A.GetRow(i).Fold((v, acc) => acc + Math.Abs(v), 0)).Max();
+            var l = 2 / norma;
+            var B = new DecimalMatrix(n);
+            var bs = new DecimalMatrix(n, 1);
+
+            int pas = 0;
+            for (int k = 0; k < p - 1; k++)
+            {
+                var sigma = (l / p) * k;
+
+                for (int i = 0; i < n; i++)
+                {
+                    B[i, i] = 1 - sigma * A[i, i];
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (i != j)
+                            B[i, j] = -sigma * A[i, j];
+                    }
+                }
+
+                for (int i = 0; i < n; i++)
+                    bs[i] = sigma * i;
+
+                var na = 1m;
+                do
+                {
+                    pas++;
+                    var y = new DecimalMatrix(n, 0m);
+                    for (int i = 0; i < n; i++)
+                    {
+                        y[i] = B.GetRow(i).Multiply(x).Sum() + bs[i];
+                    }
+                    var sum = 0m;
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                            sum += A[i, j] * (y[i] - x[i]) * (y[j] - x[j]);
+                    }
+                    na = Sqrt(sum);
+                    x = y;
+                } while (na > err);
+            }
+            Console.WriteLine(x.ToString());
+            Console.WriteLine("Iteratii: " + pas);
+        }
+
+        public static decimal Sqrt(decimal x, decimal epsilon = 0.0M)
+        {
+            if (x < 0) throw new OverflowException("Cannot calculate square root from a negative number");
+
+            decimal current = (decimal)Math.Sqrt((double)x), previous;
+            do
+            {
+                previous = current;
+                if (previous == 0.0M) return 0;
+                current = (previous + x / previous) / 2;
+            }
+            while (Math.Abs(previous - current) > epsilon);
+            return current;
+        }
+
+
+        private static DecimalMatrix GetMatrix(int n)
+        {
+            var mat = new DecimalMatrix(n);
+            mat.Iterate((it) =>
+            {
+                if (it.i == it.j)
+                    return 2 + 1 / (n * n);
+
+                if (it.j == it.i + 1 || it.i == it.j + 1)
+                    return -1;
+
+                return 0;
+            });
+            return mat;
+        }
+    }
+}
